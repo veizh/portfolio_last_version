a4ec02f039f7e83dd73bcda6c490c37f6e3a9038
Veizh <maximedurville95300@gmail.com>
2024-04-17 21:22:48 +0200

    maj

--- components/Project.jsx
+++ components/Project.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export const Project = (props)=>{
     return(
-        <Link href={"./projets?name="+props.name} className="subContainer">
+        <div href={"./projets?name="+props.name} className="subContainer">
             <img className="project" src={props.source} />
-        </Link>
+        </div>
     )
 }
